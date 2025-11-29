866b296cb261c9cc269a196a2a33bbba14ec069a
aliciawt <aliciawynonatjahjadi@gmail.com>
2025-11-29 11:28:24 +0100

    Level 200 answers

--- js1-wk1-eval/level-200.js
+++ js1-wk1-eval/level-200.js
@@ -4,13 +4,14 @@
 // Write your answers down below:
 
 // a) 10.53   For example, Math.trunc(10.53) evaluates to 10, Math.floor(10.53) evaluates to 10
-// b) 170.105
-// c) -0.34
-// d) -8.921
+// b) 170.105 Math.trunc(170.105) evaluates to 170 and Math.floor(170.105) evaulates to 170.
+// c) -0.34 Math.trunc(-0.34) evaluates to -0 and Math.floor(-0.34) evaliates to -1.
+// d) -8.921 Math.trunc(-8.921) evaluates to -8 and Math.floor(-8.921) evaluates to -9.
 
-// Part 2: Use the Node REPL to check your answers to Part 1)
+// Part 2: Use the Node REPL to check your answers to Part 1) My answers were all correct
+
 // Part 3: Using Part 2, explain the difference between Math.trunc() and Math.floor() in your own words
-
+Math.trunc() simply takes out all fraction and leaves the SVGAnimatedInteger, while Math.floor() rounds down to the smaller SVGAnimatedInteger.
 
 
 
