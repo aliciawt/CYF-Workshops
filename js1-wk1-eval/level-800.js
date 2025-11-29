5aaf49d31b971b65f8d1cdca56a2b6aa039718da
aliciawt <aliciawynonatjahjadi@gmail.com>
2025-11-29 12:04:57 +0100

    Level 800 answers

--- js1-wk1-eval/level-800.js
+++ js1-wk1-eval/level-800.js
@@ -7,4 +7,8 @@ and sharing more info on how our AI systems rank content.`
 const updatedPost = post.replaceAll('Facebook','Meta');
 
 // Use documentation to work out what updatedPost will evaluate to.
+// `Meta
+// How AI Influences What You See on Meta and Instagram
+// We’re making it easier to control what you see on Meta and Instagram, 
+// and sharing more info on how our AI systems rank content.`
 // Don't run any code for this task
