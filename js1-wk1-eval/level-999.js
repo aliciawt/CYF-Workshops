b37b2ec6028b2559c7f2aab624f4aece62524bf2
aliciawt <aliciawynonatjahjadi@gmail.com>
2025-11-29 12:24:57 +0100

    Level 999 answers

--- js1-wk1-eval/level-999.js
+++ js1-wk1-eval/level-999.js
@@ -5,8 +5,10 @@
 // write down your predictions
 // Look up documentation for Math.abs if you've not used it before
 
-console.log(Math.abs);
-console.log(Math.abs());
-console.log(Math.abs(-100));
+console.log(Math.abs); // it will return SyntaxError because no () for Math.abs
+console.log(Math.abs()); // it will return undefined because no argument inside () for Math.abs
+console.log(Math.abs(-100)); // it will return 100
 
 // Run the file with Node to check your answer
+// no. 1 answer is [Function: abs] because you can log a function without executing it, so console just prints function reference
+// no. 2 answer is NaN because argument wasn't given, thus math operation failed, thus JS produces NaN
